bc8c4592b6c351fb2840582ccca03e20f641a075
monoai <46586720+monoai@users.noreply.github.com>
2023-09-21 16:11:22 +0800

    Added ho3 answer

--- GDENG03/AppWindow.cpp
+++ GDENG03/AppWindow.cpp
@@ -1,12 +1,5 @@
 #include "AppWindow.h"
-
-struct vec3 {
-	float x, y, z;
-};
-
-struct vertex {
-	vec3 position;
-};
+#include <array>
 
 AppWindow::AppWindow()
 {
@@ -25,12 +18,42 @@ void AppWindow::onCreate()
 	RECT rc = this->getClientWindowRect();
 	m_swap_chain->init(this->m_hwnd, rc.right - rc.left, rc.bottom - rc.top);
 
-	vertex list[] = {
-		{ -0.5f, -0.5f, 0.0f },
-		{ 0.0f, 0.5f, 0.0f },
-		{ 0.5f, -0.5f, 0.0f }
+	vertex q1[] = {
+		{-0.95f,-0.5f,0.0f},
+		{-0.95f,0.5f,0.0f},
+		{-0.5f,-0.5f,0.0f },
+		{-0.5f,0.5f,0.0f}
 	};
 
+	UINT size_list1 = ARRAYSIZE(q1);
+
+	vertex q2[] = {
+		{-0.4f,-0.5f,0.0f},
+		{-0.4f,0.5f,0.0f},
+		{-0.1f,-0.5f,0.0f },
+		{-0.1f,0.5f,0.0f}
+	};
+
+	UINT size_list2 = ARRAYSIZE(q2);
+
+	vertex q3[] = {
+		{ 0.0f,-0.5f,0.0f},
+		{ 0.0f,0.5f,0.0f},
+		{ 0.4f,-0.5f,0.0f },
+		{ 0.4f,0.5f,0.0f}
+	};
+
+	UINT size_list3 = ARRAYSIZE(q3);
+
+	Quad test(q1, size_list1);
+	Quad test2(q2, size_list2);
+	Quad test3(q3, size_list3);
+
+	quads.push_back(test);
+	quads.push_back(test2);
+	quads.push_back(test3);
+	
+	/*
 	m_vb = GraphicsEngine::get()->createVertexBuffer();
 	UINT size_list = ARRAYSIZE(list);
 
@@ -41,6 +64,7 @@ void AppWindow::onCreate()
 	GraphicsEngine::get()->getShaderBufferAndSize(&shader_byte_code, &size_shader);
 
 	m_vb->load(list, sizeof(vertex), size_list, shader_byte_code, size_shader);
+	*/
 }
 
 void AppWindow::onUpdate()
@@ -53,17 +77,39 @@ void AppWindow::onUpdate()
 	//SET DEFAULT SHADER IN THE GRAPHICS PIPELINE TO BE ABLE TO DRAW
 	GraphicsEngine::get()->setShaders();
 	//SET THE VERTICES OF THE TRIANGLE TO DRAW
-	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(m_vb);
+	for (UINT i = 0; i < quads.size(); i++) {
+		GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(quads[i].getVB());
 
-	// FINALLY DRAW THE TRIANGLE
-	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleList(m_vb->getSizeVertexList(), 0);
+		// FINALLY DRAW THE TRIANGLE
+		GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleStrip(quads[i].getVB()->getSizeVertexList(), 0);
+	}
 	m_swap_chain->present(true);
 }
 
 void AppWindow::onDestroy()
 {
 	Window::onDestroy();
-	m_vb->release();
+	for (UINT i = 0; i < quads.size(); i++) {
+		quads[i].getVB()->release();
+	}
 	m_swap_chain->release();
 	GraphicsEngine::get()->release();
+}
+
+Quad::Quad(vertex list[], UINT size_list)
+{
+	m_vb = GraphicsEngine::get()->createVertexBuffer();
+
+	GraphicsEngine::get()->createShaders();
+
+	void* shader_byte_code = nullptr;
+	UINT size_shader = 0;
+	GraphicsEngine::get()->getShaderBufferAndSize(&shader_byte_code, &size_shader);
+
+	m_vb->load(list, sizeof(vertex), size_list, shader_byte_code, size_shader);
+}
+
+VertexBuffer* Quad::getVB()
+{
+	return this->m_vb;
 }
--- GDENG03/AppWindow.h
+++ GDENG03/AppWindow.h
@@ -4,6 +4,23 @@
 #include "SwapChain.h"
 #include "DeviceContext.h"
 #include "VertexBuffer.h"
+#include <vector>
+
+struct vec3 {
+	float x, y, z;
+};
+
+struct vertex {
+	vec3 position;
+};
+
+class Quad {
+public:
+	Quad(vertex list[], UINT size_list);
+	VertexBuffer* getVB();
+private:
+	VertexBuffer* m_vb;
+};
 
 class AppWindow: public Window
 {
@@ -17,6 +34,6 @@ public:
 	virtual void onDestroy() override;
 private:
 	SwapChain* m_swap_chain;
-	VertexBuffer* m_vb;
+	std::vector<Quad> quads;
 };
 
